01f1407b9926444179ab70848faa2b41a304a86e
Bharath Jagini <bharathjagini27@gmail.com>
2021-05-11 20:52:13 -0700

    py file for Breast Cancer Prediction

--- CancerPrediction.py
+++ CancerPrediction.py
@@ -40,7 +40,7 @@ class CancerPrediction:
         return np.log1p(col[0])
 
     def trainAndTestData(self,Selected_df,y):
-        X_train, X_test, y_train, y_test = train_test_split(Selected_df, y, test_size=0.2, random_state=0)
+        X_train, X_test, y_train, y_test = train_test_split(Selected_df, y, test_size=0.2, random_state=42)
         return X_train, X_test, y_train, y_test
 
     def logreg(self,X_train, X_test, y_train, y_test):
